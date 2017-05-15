531a33a83c32720f7d655c23ec92260f956a92b8
tautvydascoding <tautvydascoding@gmail.com>
2017-05-15 11:21:42 +0300

    uzduotis 8.1

--- uzduotys/uzduotis8/tautvydas/1-while/js/main.js
+++ uzduotys/uzduotis8/tautvydas/1-while/js/main.js
@@ -1,10 +1,35 @@
 // =================ARRAY / masyvas==========================
 
-
-
 console.log("labas");
- 
-// ===========================================
+
+// Uzduotis: sukurti masyva (50 ilgumo) ir uzpildyti skaiciu 0;
+var spinta = [];
+for ( i = 0; i < 50; i++) {
+  spinta[i] = 0;
+}
+console.log("Masyvas spinta: ", spinta);
+
+// uzduotis:   sukurti masyva naudojant WHILE (50 ilgumo) ir uzpildyti skaiciu 1;
+var kk = 0;
+while (kk < 50) {
+  spinta[kk] = 1;
+  kk++;
+}
+console.log("Masyvas (while): ", spinta );
+
+//------------while break--
+// uzduotis:----------------------------
+// surasti kelintas zmogus yra "Rico"
+var names = ["Enriqueta", "Sybil", "Piper", "Anh", "Carmelo", "Regan", "Synthia", "Newton", "Norbert", "Krystyna", "Fidelia", "Christoper", "Lewis", "Jeromy", "Joy", "Lorri", "Owen", "Rosenda", "Devora", "Treva", "Leanora", "Meghann", "Jacqueline", "Bunny", "Tenisha", "Rico", "Clementina", "Samella", "Clemente", "Sandi", "Efrain", "Tena", "Vivan", "Hiedi", "Naida", "Evette", "Shane", "Freida", "Marielle", "Wynona", "Cheree", "Gaston", "Aja", "Timika", "Jude", "Griselda", "Luise", "Claudie", "Minh", "Warren"];
+
+turimeVardu = names.length();
+console.log("vardu skaicius: ", turimeVardu);
+
+// uzduotis:--------------------------
+// parasyti funkcija, kuri grazina masyvo elemento numeri
+// uzduots:----------------------------
+// rasti masyve esancio zmogaus "Freida" pavarde
+// ================SWITCH===========================
 
 
 
