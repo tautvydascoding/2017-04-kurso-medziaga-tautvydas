8d1c49af56c0246c5dae5b2dfaae66b2a9644795
tautvydascoding <tautvydascoding@gmail.com>
2017-05-16 16:40:59 +0300

    uzduotis 9.2

---
+++ uzduotys/uzduotis9/tautvydas/4-obj-copy/js/main.js
@@ -0,0 +1,22 @@
+// =================ARRAY / masyvas==========================
+
+console.log("labas");
+
+
+
+
+ 
+// ===========================================
+
+
+// i konsole atspausdiname objektu masyva
+function printOgbArray(myArray) {
+  i = 0;
+   while ( i < myArray.length ) {
+     console.log("Name: " + myArray[i].name + " \n" + "lName: " +  myArray[i].lname);
+     i++;
+   }
+}
+
+
+// ===========================================
