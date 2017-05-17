070fb8f617747aebb707c43cb7366c62b16d402b
tautvydascoding <tautvydascoding@gmail.com>
2017-05-17 15:10:08 +0300

    uzduotis 10.a

--- uzduotys/uzduotis10/tautvydas/2-js-for-for-switch/js/main.js
+++ uzduotys/uzduotis10/tautvydas/2-js-for-for-switch/js/main.js
@@ -9,23 +9,31 @@ darbuotojai[0] = ["Tomas", "Petras", "Juozas", "Kestas", "Mantas"];
 darbuotojai[1] = [1969, 1960, 1930, 2000, 1996];
 darbuotojai[2] = [13, 20, 51, 3, 6];
 
-console.log(darbuotojai[0].join( ", "));
+// console.log(darbuotojai[0].join( ", "));
 
 // uzdavinys: naudojant tik : "darbuotojai" masyva ir for for ciklus, isvesti i konsole matricos duomenis:
 // Tomas   1969   13
 // Petras  1960   20
 
-for (i = 0; i < ; i++) {
-  for ( k = 0;) {
-    console.log(darbuotojai[0][2]);
+for (i = 0; i < 5; i++) {    // < darbuotojai[0].length
+      k = 0;
+      console.log(darbuotojai[k][i], " ", darbuotojai[k+1][i], " ", darbuotojai[k+2][i] );
+}
+
+for (i = 0; i < 5; i++) {    // < darbuotojai[0].length
+  var eilete = "";  
+  for ( k = 0; k <  darbuotojai.length; k++) {
+       eilete = eilete + " " + darbuotojai[k][i];  // eilete +=  darbuotojai[k][i];
   }
+  console.log(eilete);
 }
 
 // =================svarbus patarimai===================:
 // ARRAY: jeigu naudoji FOR || while cikla, tai negalvojant masyve naudoti "i" (pvz.: myArray[i])
-// FOR:   "for (i = 0; i < ; i++) {"  - nesusijusi su uzdaviniu (skirtas apeit masyva/ ivykdyti daug kartu kazka)
+// FOR:   "for (i = 0; i < x; i++) {"  - nesusijusi su uzdaviniu (skirtas apeit masyva/ ivykdyti daug kartu kazka)
 // matricas  - visada nusipiesti ant popieriaus eskiza su testiniais duomenimis
 // matrica narys paimamas: myMatica[x][y]   x- stulpelis; y - eilute
+// matrica - pasitikritni ar nereik x ir y sukeisti vietomis (ar nesuklydai kuris stulpelis ir kuris eilute )
 // matricos ilgis daznai skiriasi nuo plocio.
 //  pvz.:
  // for ( i < eiluciuSkaicius)
