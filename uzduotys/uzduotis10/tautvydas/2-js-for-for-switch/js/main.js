595f746cb75edf141fddc6d1c045cf8770dee4eb
tautvydascoding <tautvydascoding@gmail.com>
2017-05-17 13:34:23 +0300

    uzduotis 10.a

--- uzduotys/uzduotis10/tautvydas/2-js-for-for-switch/js/main.js
+++ uzduotys/uzduotis10/tautvydas/2-js-for-for-switch/js/main.js
@@ -4,6 +4,32 @@ console.log("labas");
 
 //============for for========================
 
+var darbuotojai =[];
+darbuotojai[0] = ["Tomas", "Petras", "Juozas", "Kestas", "Mantas"];
+darbuotojai[1] = [1969, 1960, 1930, 2000, 1996];
+darbuotojai[2] = [13, 20, 51, 3, 6];
+
+console.log(darbuotojai[0].join( ", "));
+
+// uzdavinys: naudojant tik : "darbuotojai" masyva ir for for ciklus, isvesti i konsole matricos duomenis:
+// Tomas   1969   13
+// Petras  1960   20
+
+for (i = 0; i < ; i++) {
+  for ( k = 0;) {
+    console.log(darbuotojai[0][2]);
+  }
+}
+
+// svarbus patarimas:
+// jeigu naudoji FOR || while cikla, tai negalvojant masyve naudoti "i" (pvz.: myArray[i])
+// patarimas: FOR "for (i = 0; i < ; i++) {"  - nesusijusi su uzdaviniu
+// matricas  - nusipiesti ant popieriaus
+// matrica narys paimamas: myMatica[x][y]   x- stulpelis; y - eilute
+// matricos ilgis daznai skiriasi nuo plocio.
+//  pvz.:
+ // for ( i < eiluciuSkaicius)
+ //    for ( k < stulpeliuSkaicius)
 
 // ================SWITCH===========================
 
