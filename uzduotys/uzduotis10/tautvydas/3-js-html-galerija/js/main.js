604d66673348f45d3c1d199841703f82a6d706c1
tautvydascoding <tautvydascoding@gmail.com>
2017-05-17 15:57:40 +0300

    uzduotis 10.a

--- uzduotys/uzduotis10/tautvydas/3-js-html-galerija/js/main.js
+++ uzduotys/uzduotis10/tautvydas/3-js-html-galerija/js/main.js
@@ -2,11 +2,27 @@
 
 console.log("labas");
 
- 
+
 // ===========================================
+// uzduotis: isvesti dvi eilutes po 4 paveiksliukus i HTML
 
+document.write("<h2> Galerija </h2>");
 
+var img = '<img src="http://www.pngall.com/wp-content/uploads/2016/04/Tomato-PNG-Clipart.png";  alt="pomidoras raudonas apvalus" width="10%">  ';
 
+// for (t = 0; t < 8; t++) {
+//     document.write(img);
+//     if ( t === 3 ) {
+//       document.write("<br>");
+//     }
+// }
 
+//-----
 
+for (t = 0; t < 5; t++) {
+    for (g = 0 ; g < 4 ; g++) {
+        document.write(img);
+    }
+    document.write("<br>");
+}
 // ===========================================
