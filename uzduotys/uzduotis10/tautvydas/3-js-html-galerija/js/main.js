6d30bd76e4b2dfb2aac5da1e1ddd89a2b39e8d53
tautvydascoding <tautvydascoding@gmail.com>
2017-05-17 17:08:32 +0300

    uzduotis 10.a

--- uzduotys/uzduotis10/tautvydas/3-js-html-galerija/js/main.js
+++ uzduotys/uzduotis10/tautvydas/3-js-html-galerija/js/main.js
@@ -19,10 +19,31 @@ var img = '<img src="http://www.pngall.com/wp-content/uploads/2016/04/Tomato-PNG
 
 //-----
 
-for (t = 0; t < 5; t++) {
-    for (g = 0 ; g < 4 ; g++) {
-        document.write(img);
-    }
-    document.write("<br>");
+// for (t = 0; t < 5; t++) {
+//     for (g = 0 ; g < 4 ; g++) {
+//         document.write(img);
+//     }
+//     document.write("<br>");
+// }
+
+var galerija = document.querySelector(".gallery");
+galerija.style.backgroundColor = "red";
+// galerija.appendChild(img);
+galerija.innerHTML += img;
+
+for (t = 0; t < 8; t++) {
+    galerija.innerHTML += img;
 }
+
 // ===========================================
+// uzduotis: pakeisti h2 elemeneto spalva i melyna (naudojant tik js)
+
+document.querySelectorAll("h2")[0].style.color = "blue";
+// document.getElementById("id")
+// document.getElementByClass("klase")
+// documentgetElement("h2")
+
+document.write("<h3> Testatvimas</h3>");    // sukuremas ir idedamas i HTML
+var button = document.createElement("div"); // sukuremas, bet Neidedamas i HTML
+button.innerHTML = "<button type='text' name='OK' value='Spausk'>";
+galerija.appendChild(button);
