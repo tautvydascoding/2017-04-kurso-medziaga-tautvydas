3ec6e57c5d3291998ae335155aa7a7376a384bb4
tautvydascoding <tautvydascoding@gmail.com>
2017-05-18 13:21:38 +0300

    uzduotis 11.1

--- uzduotys/uzduotis11/tautvydas/2-jQ-effectai/js/main.js
+++ uzduotys/uzduotis11/tautvydas/2-jQ-effectai/js/main.js
@@ -3,10 +3,32 @@
 console.log("labas");
 
 $(document).ready(function() {
-  $('h1').css("color", "purple");
+  $('h1').css("color", "red");
 
- 
 
+  // img
+  var img = '<img src="http://www.pngall.com/wp-content/uploads/2016/04/Tomato-PNG-Clipart.png";  alt="pomidoras raudonas apvalus" width="10%">  ';
+
+  $(".gallery").html( getXimages(3) );
+
+  // $('img').hide(4000); // skaicisu nurodo mikro sekundes, per keik laiko ivyks veiksmas
+  // $('.gallery > img:first').show(1500);
+
+// uzduotis : pasletpi paveiksliuka tik ta kur paspaudi
+  $('img').click( function() {
+      $(this).hide(2000);
+      $('footer').css("height", "20px");
+  } );
+
+
+
+
+  function getXimages(x) {
+     for (i  =0; i < x; i++ ){
+       img += '<img src="http://www.pngall.com/wp-content/uploads/2016/04/Tomato-PNG-Clipart.png";  alt="pomidoras raudonas apvalus" width="10%">  ';
+     }
+     return img;
+  }
 });
 
 
