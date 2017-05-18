89bfaa6eb4a89739d15f91d68faea5a936cc781f
tautvydascoding <tautvydascoding@gmail.com>
2017-05-18 15:02:45 +0300

    uzduotis 11.1

--- uzduotys/uzduotis11/tautvydas/2-jQ-effectai/js/main.js
+++ uzduotys/uzduotis11/tautvydas/2-jQ-effectai/js/main.js
@@ -20,8 +20,22 @@ $(document).ready(function() {
       $('footer').css("height", "20px");
   } );
 
-
-
+// -------F-JOS--CALL BACK---
+function suma(x, y, myFunction) {
+  var ats = x + y;
+  myFunction(ats); //  spasudina
+}
+var print = function(z) {
+  console.log("Suma: " + z);
+};
+suma(10, 5, print);
+// -----------BE CALL BACK-------------------
+function suma2(x, y) {
+  var ats = x + y;
+  return ats;
+}
+kk = suma2(10,5);
+print( kk );
 
   function getXimages(x) {
      for (i  =0; i < x; i++ ){
