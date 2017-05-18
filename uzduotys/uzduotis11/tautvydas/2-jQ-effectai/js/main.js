3b6715275ee1b64bbc3cd87cd4c401070842bad6
tautvydascoding <tautvydascoding@gmail.com>
2017-05-18 12:17:57 +0300

    uzduotis 11.1

---
+++ uzduotys/uzduotis11/tautvydas/2-jQ-effectai/js/main.js
@@ -0,0 +1,13 @@
+// =================ARRAY / masyvas============
+
+console.log("labas");
+
+$(document).ready(function() {
+  $('h1').css("color", "purple");
+
+ 
+
+});
+
+
+// ===========================================
