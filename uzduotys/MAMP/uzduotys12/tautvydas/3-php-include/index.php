f8d948de1deb0797b4e4730af3e10788c96fbcb1
tautvydascoding <tautvydascoding@gmail.com>
2017-05-19 15:19:28 +0300

    uzduotis 12.a

--- uzduotys/MAMP/uzduotys12/tautvydas/3-php-include/index.php
+++ uzduotys/MAMP/uzduotys12/tautvydas/3-php-include/index.php
@@ -12,7 +12,7 @@
 <body>
 <!-- ---------header ------ -->
  <?php
-  include("header.html");
+  include_once("header.html");
   ?>
 
 <!-- ---------content ------ -->
---
+++ uzduotys/MAMP/uzduotys12/tautvydas/4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test PHP</title>
+  </head>
+  <body>
+      <h1>Test PHP arrays</h1>
+
+      <?php
+          echo "<h2> Labas</h2>";
+          echo "labas" ;
+
+       ?>
+
+  </body>
+</html>
