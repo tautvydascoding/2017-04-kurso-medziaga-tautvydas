c8382934595050728a4cf2744fce6c11cd5522c0
tautvydascoding <tautvydascoding@gmail.com>
2017-05-19 12:38:40 +0300

    uzduotis 12.a

--- uzduotys/MAMP/uzduotys12/tautvydas/2/index.php
+++ uzduotys/MAMP/uzduotys12/tautvydas/2/index.php
@@ -1,5 +1,30 @@
 labas
 <?php
-    echo  "viso gero";
+    echo  "viso gero <br><br><br>";
 
+
+    $name = "Tadas";
+    $newName  = "Petras";
+
+    echo "Vardas $name <br>";
+    $name = $newName;
+    echo "Vardas $name <br>";
+
+
+    $x = "Pakeistas";
+
+    $name = &$x;
+    echo "Vardas po (atminties adreso priskyrimo) $name <br>";
+    $x = "Dundulis";
+    echo "Vardas po (x pakeitimo) $name <br>";
+
+
+    function skaiciuotiKvadratu($k) {
+      $k = $k * $k;
+      return $k;
+    }
+
+
+
+    echo "2 kavadratu: " .  skaiciuotiKvadratu(2);
  ?>
