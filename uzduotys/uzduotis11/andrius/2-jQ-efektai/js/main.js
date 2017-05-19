899c4c79c515469cb45bb17047de10b138eab416
kondratas <kondratas.an@gmail.com>
2017-05-19 10:37:09 +0300

    11uzduotis

--- uzduotys/uzduotis11/andrius/2-jQ-efektai/js/main.js
+++ uzduotys/uzduotis11/andrius/2-jQ-efektai/js/main.js
@@ -1,4 +1,4 @@
-console.log("labas");
+console.log("labas2");
 
 $(document).ready(function() {
   $("h1").css("color", "red");
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 
   //========================================2 budas
-$(".gallery").append("<div class=div1></div>"); // sukuriaus div'e su klase gallery nauja div'a su klase div1
+  $(".gallery").append("<div class=div1></div>"); // sukuriaus div'e su klase gallery nauja div'a su klase div1
   $(".gallery .div1").html(getXimages(4)); //paleidziam funkcija getXimages ir isvedam img i ekrana keturis kartus siuo atveju, galima dar naudot vietoje .html --> .append tada ne ant virsaus uzdes, o salia
 
 
@@ -22,10 +22,38 @@ $(".gallery").append("<div class=div1></div>"); // sukuriaus div'e su klase gall
 
   //===========uzduotis: paslepti paveiksliuka tik ta kur paspaudi
 
-$("img").click(function() {
-  $(this).hide(2000);
-  // $("footer").css("height", "20px");
-});
+  $("img").click(function() {
+    $(this).hide(2000);
+    // $("footer").css("height", "20px");
+  });
+
+$("header").css("display", "none");
+  $("button").on("click", function() {
+    $("header").slideToggle(1000);
+  });
+
+//uzduotis: uzvedus pele ant nuotraukos ji turi tapti 50% permatoma, nuvedus vel turi tapti 100%
+// 1vienas budas
+// $("img").on({
+//     mouseenter: function(){
+//         $(this).css("opacity", "0.5");
+//     },
+//     mouseleave: function(){
+//         $(this).css("opacity", "1");
+//     },
+//   });
+// 2antras budas
+$("img").on({
+    mouseenter: function(){
+        $(this).fadeTo("300", "0.5");
+    },
+    mouseleave: function(){
+        $(this).fadeTo("0", "1");
+    },
+  });
+
+
+
 
 
 
@@ -43,5 +71,25 @@ $("img").click(function() {
 
 
 
+
+
+  //========================FUNKCIJOS CALLBACK===========================
+  //=======SU CALLBACK======
+
+  // function suma(x, y, myFunction) {
+  //   var ats = x + y;
+  //   myFunction(ats);
+  // }
+  // var print = function(z) {
+  //   console.log("Suma: " + z);
+  // };
+  // suma(10, 5, print);
+  //=======BE CALLBACK======
+  // function suma2(x, y) {
+  //   var ats = x + y;
+  //   return ats;
+  // }
+  // kk = suma2(11, 5);
+  // print(kk);
 
 });
