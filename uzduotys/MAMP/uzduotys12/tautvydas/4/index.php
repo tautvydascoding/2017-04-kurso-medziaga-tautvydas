97e252b17b85b3b215d60f3de380c5707eca685c
tautvydascoding <tautvydascoding@gmail.com>
2017-05-19 15:51:31 +0300

    uzduotis 12.a

--- uzduotys/MAMP/uzduotys12/tautvydas/4/index.php
+++ uzduotys/MAMP/uzduotys12/tautvydas/4/index.php
@@ -11,7 +11,7 @@
           echo "<h2> Labas</h2>";
           echo "labas <br />" ;
 
-          $humans = ["name" => "Tom",
+          $humans =  ["name" => "Tom",
                     "lname" => "Okpvol",
                         1996, 2, 16];
           print_r($humans);
@@ -20,6 +20,12 @@
           foreach($humans as $human) {
             echo "Reiksme: $human  <br />";
           }
+
+          $article = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+          $words = explode(" ", $article);
+          print_r($words);
+
        ?>
 
   </body>
