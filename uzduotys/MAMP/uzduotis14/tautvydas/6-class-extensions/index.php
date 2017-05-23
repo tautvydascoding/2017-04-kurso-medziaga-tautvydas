1c569bc852c59da8b821df03bc743edeb6e7bc84
tautvydascoding <tautvydascoding@gmail.com>
2017-05-23 16:03:50 +0300

    paskaita 14.0

--- uzduotys/MAMP/uzduotis14/tautvydas/6-class-extensions/index.php
+++ uzduotys/MAMP/uzduotis14/tautvydas/6-class-extensions/index.php
@@ -10,19 +10,50 @@
     <?PHP
 
     // uzduotis:
-    // 1) sukurti interface "Edit" : turi funkcijas, kurios nera aprasytos (privalomos)!
+    // 1) sukurti interface "Edit" : kuri turi funkcijas: (kurios nera aprasytos (privalomos)!
+        // 0) Ar gali tureti kintamuosius????
         // changeName($a)
         // isLogin()
         // getName()
     // 2) sukurti (abstrakcia) klase "User" :
-        // A) kuri turi privalomus kintamuosius:
-            // name, password
-        // B) neprivalomus kintamuosius:
-            //  createdDate, lastLogin
-        // C) privaloma funkcija "Loguot()"
-        // D) neprivaloma funkcija clearlastLoginTime()
+        // 0) abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
+        // A) kuri turi neprivalomus kintamuosius:
+            // name, password , createdDate, lastLogin
+        // B) privaloma funkcija "Loguot()"
+        // C) neprivaloma funkcija clearlastLoginTime()
     // 3) sukurti klase Admin :
         // A)  kuri panaudoja Edit ir User ~klases (implementuoti ir paveldeti)
+
+        interface Edit {
+          // interface negali tureti KINTAMUJU
+          public function changeName($a);
+          public function isLogin();
+          public function getName();
+        }
+        abstract class User {
+          // abstrakti kalse - NEGALI TURETI ABSTRAKCIU KINTAMUJU (BET PAPRASTUS GALI)
+          public $name, $password , $createdDate, $lastLogin;
+
+          abstract public function  Loguot();   // ja tures realizuoti kazkas kitas
+          public function clearlastLoginTime() {
+            echo "Jusu duomensy isvalys";
+          }
+        }
+        class Admin extends User implements Edit {
+            public function Loguot() {
+              echo "Jus atsjungtas!";
+            }
+            public function changeName($a) {
+                $this->name = $a;
+            }
+            public function isLogin() {
+                // kazkoks kodas
+            }
+            public function getName(){
+                return $this->name;
+            }
+        }
+
     ?>
 
 
