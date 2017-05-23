1347faca3f1f029ffc68a4438e14f3e58bae0ec0
tautvydascoding <tautvydascoding@gmail.com>
2017-05-23 14:28:00 +0300

    paskaita 14.0

--- uzduotys/MAMP/uzduotis14/tautvydas/6-class-extensions/index.php
+++ uzduotys/MAMP/uzduotis14/tautvydas/6-class-extensions/index.php
@@ -7,73 +7,14 @@
   <body>
     <h1>abstract class && Interface</h1>
 
-    <?php
+    <?PHP
 
-      interface Human {
-        public function getName( ) ;
-        public function getPersonalCode() ;
-      }
-      class Man implements Human {
-        private $name = "Test";
-        private $testosterone = 3232;
-        public function snore() {
-        }
-        public function getName() {
-          return $this->name;
-        }
-        // mes kalida nes Overloding neveikia PHP
-        // public function getName($aa) {
-        // }
-        public function setName($text) {
-          $this->name = $text;
-        }
-        public function getPersonalCode() {}
-      }
-      $juozas = new Man;
-      echo $juozas->getName()."<br />";
-      $juozas->setName("Juozas");
-      echo $juozas->getName()."<br />";
+    // uzduotis:
+    // 
+
+    ?>
 
 
-      abstract class HumanType {
-        public $skinType;
-        private $religion;
-        abstract function getSpeed($asaa) ;
-      }
-      class Woman extends HumanType {
-        public $skinType = "#f25631";
-        private $estrogenai = 31654;
-        function getSpeed($text) {
-          // kazkoks kodas
-        }
-      public function gimdymas() {
-          $this->sumazintiEstro();
-          echo "sumazino Estrog.";
-        }
-        private function sumazintiEstro() {
-          $this->estrogenai -= 10;
-        }
-        private function padidintiEstro() {
-            $this->estrogenai =  $this->estrogenai + 10;
-        }
-      }
-      class Test extends Woman {
-        public function gimdymas() {
-          echo "isvaziuoju";
-        }
-      }
-      $test = new Test;
-      $test->gimdymas()."<br />";
+    </body>
 
-      $Marija = new Woman;
-      $Marija->gimdymas();
-      // $Marija->sumazintiEstro(); // privaciu funkciju neiseina issaukti!
-
-
-      
-
-
-     ?>
-
-  </body>
-</html>
+  </html>
