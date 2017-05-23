5b84832cfd54de97d82e0690f3fcf3f62b3d105a
tautvydascoding <tautvydascoding@gmail.com>
2017-05-23 11:47:02 +0300

    paskaita 14.0

--- uzduotys/MAMP/uzduotis14/tautvydas/5-class-extends/index.php
+++ uzduotys/MAMP/uzduotis14/tautvydas/5-class-extends/index.php
@@ -22,19 +22,43 @@
             }
 
             class Liutas extends Tipas {
-              public $kailis = true;
-              public $spalva = "#a34baa";
-              public function medzioti() {
-                // kazkoks kodas
-              }
-              private  function iToleta() {
-                // kazkoks kodas
-              }
+                private $name;
+                public $kailis = true;
+                public $spalva = "#a34baa";
+                public function medzioti() {
+                  // kazkoks kodas
+                }
+                private  function iToleta() {
+                  // kazkoks kodas
+                }
+                public function setName($xx) {
+                    $this->name = $xx;
+                }
+                public function getName() {
+                    return $this->name;
+                }
             }
 
+            $simba = new Liutas();
+            $hdaras = new Liutas();
+            $lita = new Liutas();
 
+            // uzduotis 1: pakeisti visu triju liutu spalvas (ir pakeitimus atspausdinti)
+              // $simba->spalva = "violetine";
+              // $hdaras->spalva = "juodas";
+              // $lita->spalva = "ruda";
+              //
+              // echo "Simbos spalva: " . $simba->spalva . " <br />";
+              // echo "Hdarro spalva: " . $hdaras->spalva . " <br />";
+              // echo "Litos spalva: " . $lita->spalva . " <br />";
+              // print_r($simba);  // atspausdina visa obj.
 
+              // uzduotis2: Liuto klasei susikurti privatu kintamaji "Name". ir uskurti 2 funkcijas jo reiksmei pakeisti ir paimti "setName($x)", "getName()".
 
+              $simba->setName("Karolis");
+              echo " Simbos vardas: " . $simba->getName() . "<br />";
+              $simba->setName("Ana");
+              echo " Simbos vardas: " . $simba->getName() . "<br />";
 
        ?>
 
