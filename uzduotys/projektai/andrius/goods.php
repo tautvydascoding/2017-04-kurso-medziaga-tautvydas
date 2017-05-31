d94bc5d21d9b0491a959b1e05142172ac3026baf
kondratas <kondratas.an@gmail.com>
2017-05-31 16:32:07 +0300

    proejektas

---
+++ uzduotys/projektai/andrius/goods.php
@@ -0,0 +1,62 @@
+<div class="contentHolder">
+  <h2 class="text-center">Mūsų produkcija</h2>
+  <div class="pageContent">
+    <!-- karusele pradzia -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+
+        <div class="item active">
+          <img src="img/produkcija/bulves.jpg" alt="bulvės" width="460" height="345">
+          <div class="carousel-caption">
+            <h3>Šviežios bulvės "Adora"</h3>
+            <p>Kai tik turėsime, būtinai - pranešime!!</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/produkcija/morkos.jpg" alt="Morkos" width="460" height="345">
+          <div class="carousel-caption">
+            <h3>Morkos</h3>
+            <p>Morkos dar gyvena po žeme &#x1f60a;</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/produkcija/burokai.jpg" alt="Burokai" width="460" height="345">
+          <div class="carousel-caption">
+            <h3>Burokėliai</h3>
+            <p>O burokėliai kaip ir morkos...</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="img/produkcija/kopustai.jpg" alt="Kopūstai" width="460" height="345">
+          <div class="carousel-caption">
+            <h3>Kopūstai</h3>
+            <p>Kopūstai kopūstėliai, šiuo metu dar auga</p>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <!-- karusele pabaiga -->
---
+++ uzduotys/projektai/andrius/js/main.js
@@ -0,0 +1,24 @@
+console.log("labasprojektas55555");
+$(document).ready(function() {
+
+    $('#fullpage').fullpage({
+      fixedElements: 'header, footer',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+      navigation: true,
+      scrollOverflow: true
+      });
+
+
+
+
+});
+
+
+// meniu js nes kitaip nemato
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
